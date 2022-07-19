9c9d15dcf75e886ce64e9e18682f829593001ebc
Erkin Isoyev <githubuser112@gmail.com>
2022-07-19 09:30:58 +0500

    initial

---
+++ my_first_code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    sqrt()
+    pr//cpptools-extension-pack/1.2.0/1651852580572/Microsoft.VisualStudio.Services.Icons.Defaultintf("Hello world!\n");
+
+    return 0;
+}
