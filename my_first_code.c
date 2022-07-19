3f2a78d9d78d6e08ca3b2acc8398de495c97d389
Erkin Isoyev <githubuser112@gmail.com>
2022-07-19 09:38:26 +0500

    new print

--- my_first_code.c
+++ my_first_code.c
@@ -2,8 +2,7 @@
 #include <math.h>
 
 int main(){
-    sqrt()
-    pr//cpptools-extension-pack/1.2.0/1651852580572/Microsoft.VisualStudio.Services.Icons.Defaultintf("Hello world!\n");
-
+    
+    printf("Hello world");
     return 0;
 }
